915a0451ac9c055a5f1992b385b0746f4203e222
SetSunHeng <sunhengbrokh270405@gmail.com>
2026-01-28 12:31:13 +0700

    change

--- lib/common/widgets/custom_shap/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shap/containers/primary_header_container.dart
@@ -1,15 +1,10 @@
-
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import '../../../../utils/constants/colors.dart';
 import '../curved_edges/curve_edges_widget.dart';
 import 'circle_container.dart';
 
 class SPrimaryHeaderContainer extends StatelessWidget {
-  const SPrimaryHeaderContainer({
-    super.key, required this.child,
-  });
+  const SPrimaryHeaderContainer({super.key, required this.child});
 
   final Widget child;
 
@@ -21,9 +16,36 @@ class SPrimaryHeaderContainer extends StatelessWidget {
         padding: const EdgeInsets.all(0),
         child: Stack(
           children: [
-            Positioned(top: -170, left: -160, child: TRoundedContainer(height: 300, width: 300, backgroundColor: TColors.primaryBackground.withOpacity(0.2), radius: 200)),
-            Positioned(top: -150, right: -200, child: TRoundedContainer(height: 400, width: 400, backgroundColor: TColors.white.withOpacity(0.1), radius: 400)),
-            Positioned(top: 100, right: -250, child: TRoundedContainer(height: 350, width: 350, backgroundColor: TColors.white.withOpacity(0.1), radius: 200)),
+            Positioned(
+              top: -170,
+              left: -160,
+              child: TRoundedContainer(
+                height: 300,
+                width: 300,
+                backgroundColor: TColors.primaryBackground.withOpacity(0.2),
+                radius: 200,
+              ),
+            ),
+            Positioned(
+              top: -150,
+              right: -200,
+              child: TRoundedContainer(
+                height: 400,
+                width: 400,
+                backgroundColor: TColors.white.withOpacity(0.1),
+                radius: 400,
+              ),
+            ),
+            Positioned(
+              top: 100,
+              right: -250,
+              child: TRoundedContainer(
+                height: 350,
+                width: 350,
+                backgroundColor: TColors.white.withOpacity(0.1),
+                radius: 200,
+              ),
+            ),
             child,
           ],
         ),
